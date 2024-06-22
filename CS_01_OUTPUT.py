1bd4242010187668456ba03e39dfc3b8dcecf83a
2100039033 <137202274+2100039033@users.noreply.github.com>
2024-06-22 21:40:40 +0530

    Add files via upload

---
+++ CS_01_OUTPUT.py
@@ -0,0 +1,7 @@
+"C:\Users\Nihitha Narreddy\PycharmProjects\Prodigy\venv\Scripts\python.exe" "C:\Users\Nihitha Narreddy\PycharmProjects\Prodigy\CS_01.py"
+Would you like to encrypt or decrypt the message? (e/d): E
+Enter the message: PRODIGY
+Enter the shift value: 5
+Encrypted message: UWTINLD
+
+Process finished with exit code 0
